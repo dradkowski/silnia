dc095de700a1d46db741764bae8566562e9c10af
dradkowski <cyberdogpolska@gmail.com>
2018-10-25 12:26:14 +0200

    silnia 1

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, n, silnia = 1;
+    printf("podaj n: ");
+    scanf(" %i", &n);
+
+    if (n > 0 && n < 15){
+        for (i = 1; i <= n; i++){
+            silnia = silnia * i;
+        }
+        printf("%i", silnia);
+    } else {
+        printf("podaj n w przedzialne od 0 to 15");
+    }
+    
+    return 0;
+}
